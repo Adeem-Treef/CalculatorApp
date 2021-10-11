eef8e639d7bbc386852ca245ef3f1a5ca0266502
Qudus Musiliu <58162408+Adeem-Treef@users.noreply.github.com>
2021-10-11 10:41:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,123 +1,302 @@
-window.onload = function () {
-    if (parseInt(document.querySelector("body > div.calculator > div.calculator-heading > div > div.slider-level > input").defaultValue) === 0) chooseSlide();
-}
+w// Elements
+const keyContainer = document.querySelector('.key-items');
+const toggleBar = document.querySelector('.toggle-bar');
+const content = document.querySelector('.content');
+const error = document.querySelector('.error');
 
-let keysArray = [];
+class Calculator {
+  #contentWidth;
+  #currOperation = null;
+  #currNumber = null;
+  #prevNumber = null;
+  #numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  #others = {
+    dot: '.',
+    eq: '=',
+    plus: '+',
+    min: '-',
+    mul: 'x',
+    div: '/',
+    del: 'del',
+    res: 'reset',
+  };
+  #operations = {
+    plus(a, b) {
+      return a + b;
+    },
+    min(a, b) {
+      return a - b;
+    },
+    mul(a, b) {
+      return a * b;
+    },
+    div(a, b) {
+      return a / b;
+    },
+  };
 
-function applythemeCss(bodyCss, inputCss, screenCss, keypadCss, cssDelReset, equalCss, toggleCss, toggleSliderCss) {
-    document.querySelector("body").style.cssText = bodyCss;
-    var inputs = document.querySelectorAll(".button-row > input[type=button]");
-    for (let i = 0; i < inputs.length; i++) {
-        inputs[i].style.cssText = inputCss;
-    }
-    document.querySelector("body > div.calculator > div.calculator-screen").style.cssText = screenCss;
-    document.querySelector("body > div.calculator > div.calculator-body").style.cssText = keypadCss;
-    document.querySelector("body > div.calculator > div.calculator-body > div.button-row.one > input.del").style.cssText = cssDelReset;
-    document.querySelector("body > div.calculator > div.calculator-body > div.button-row.five > input.reset").style.cssText = cssDelReset;
-    document.querySelector("body > div.calculator > div.calculator-body > div.button-row.five > input.equal").style.cssText = equalCss;
-    document.querySelector("body > div.calculator > div.calculator-heading > div > div.slider-level > input").style.cssText = toggleCss;
-    document.querySelector("body > div.calculator > div.calculator-keyboard-rules").style.cssText = keypadCss;
-}
-function chooseSlide() {
-    var value = parseInt(document.querySelector("body > div.calculator > div.calculator-heading > div > div.slider-level > input").value);
-    console.log(`Theme Level: ${value + 1}`);
-    let bodyCss, inputCss, screenCss, keypadCss, cssDelReset, equalCss, toggleCss;
+  constructor() {
+    this.#generateMarkup();
+    this.#contentWidth = content.scrollWidth;
+    toggleBar.addEventListener('click', this.#switchThemes);
+    keyContainer.addEventListener('click', this.#handleKeys.bind(this));
+    window.addEventListener('keydown', this.#bindKeyboard.bind(this));
+  }
 
-    if (value === 0) {
-        bodyCss = "background-color:hsl(222, 26%, 31%);color:#fff;";
-        inputCss = "background-color:hsl(30, 25%, 89%);box-shadow:0px 3px hsl(32, 16%, 65%);color:hsl(221, 14%, 31%);";
-        screenCss = "background-color:hsl(224, 36%, 15%);";
-        keypadCss = "background-color:hsl(223, 31%, 20%);";
-        cssDelReset = "background-color:hsl(285, 91%, 52%) !important;box-shadow: 0px 3px hsl(281deg 89% 26%) !important;color:#fff !important;";
-        equalCss = "background-color:hsl(6, 63%, 50%) !important;box-shadow: 0px 3px hsl(6, 70%, 34%) !important;color:#fff !important;"
-        toggleCss = "background-color:hsl(223, 31%, 20%);";
-        applythemeCss(bodyCss, inputCss, screenCss, keypadCss, cssDelReset, equalCss, toggleCss);
-    } else if (value === 1) {
-        bodyCss = "background-color:hsl(0, 0%, 90%);color:hsl(60, 10%, 19%);";
-        inputCss = "background-color:hsl(45, 7%, 89%);box-shadow:0px 3px hsl(35, 11%, 61%);color:hsl(60, 10%, 19%);";
-        screenCss = "background-color:hsl(0, 0%, 93%);";
-        keypadCss = "background-color:hsl(0, 5%, 81%)";
-        cssDelReset = "background-color:hsl(185, 42%, 37%)!important;box-shadow: 0px 3px hsl(185, 58%, 25%) !important;color:#fff;";
-        equalCss = "background-color:hsl(25, 98%, 40%) !important;box-shadow: 0px 3px hsl(25, 99%, 27%) !important;color:#fff;";
-        toggleCss = "background-color:hsl(0, 5%, 81%);";
-        applythemeCss(bodyCss, inputCss, screenCss, keypadCss, cssDelReset, equalCss, toggleCss);
-    } else if (value === 2) {
-        bodyCss = "background-color:hsl(268, 75%, 9%);color:hsl(52, 100%, 62%);";
-        inputCss = "background-color:hsl(268, 47%, 21%);box-shadow:0px 3px hsl(290, 70%, 36%);color:hsl(52, 100%, 62%);";
-        screenCss = "background-color:hsl(268, 71%, 12%);";
-        keypadCss = "background-color:hsl(268, 71%, 12%);";
-        cssDelReset = "background-color:hsl(281, 89%, 26%)!important;box-shadow: 0px 3px hsl(285, 91%, 52%) !important;color:#fff;";
-        equalCss = "background-color:hsl(176, 100%, 44%) !important;box-shadow: 0px 3px hsl(177, 92%, 70%) !important;color:hsl(221, 14%, 31%);";
-        toggleCss = "background-color:hsl(268, 71%, 12%);";
-        applythemeCss(bodyCss, inputCss, screenCss, keypadCss, cssDelReset, equalCss, toggleCss);
+  #insertMarkup(type, dataValue, gridArea, value) {
+    const markup = `
+      <li
+        class="small-key ${type}"
+        data-value="${dataValue}"
+        style="grid-area: ${gridArea}"
+      >
+        ${value}
+      </li>`;
+    keyContainer.insertAdjacentHTML('beforeend', markup);
+  }
+
+  #generateMarkup() {
+    this.#numbers.forEach(n => this.#insertMarkup('number', n, `n-${n}`, n));
+    Object.entries(this.#others).forEach(val =>
+      this.#insertMarkup(val[0], val[0], val[0], val[1])
+    );
+    content.focus();
+  }
+
+  #switchThemes(e) {
+    const radio = e.target.closest('input');
+    if (!radio) return;
+    document.body.className = `theme-${radio.value}`;
+  }
+
+  #renderError(msg) {
+    error.textContent = '';
+    error.textContent = msg;
+    error.classList.remove('hidden');
+    setTimeout(function () {
+      error.classList.add('hidden');
+    }, 1500);
+  }
+
+  #setplaceholder() {
+    const prevNumber = this.#addCommas(this.#prevNumber.toString());
+    const number =
+    prevNumber.length > 18 ? this.#prevNumber.toExponential(3) : prevNumber;
+    content.value = number + 'over';
+
+    if (content.scrollWidth > this.#contentWidth) {
+      content.style.fontSize = '1.5rem';
     } else {
-        console.log("Theme doesn't exists");
+      content.style.fontSize = '2rem';
     }
-}
-function keyPress(event)
-{
-    let regex = /[0-9]|[.,+-/*]/;
-    // used , as an alternate for √ which wasn't available on the keyboard i was working on.
-    if(regex.test(event.key)){
-        if(event.key === ','){
-            displayKeys('√');
-        } else {
-            displayKeys(event.key);
-        }
-    } else if (event.key === "Backspace"){
-        deleteKey();
-    }else if (event.key === "Delete"){
-        resetKeys();
-    } else if (event.key === "=" || event.key === "Enter"){
-        getResult();
+
+    content.value = '';
+    content.setAttribute('placeholder', number);
+    
+  }
+
+  #remove() {
+    if (
+      content.value.length === 0 &&
+      content.getAttribute('placeholder').length === 0
+    ) {
+      return;
+    }
+
+    let number;
+    if (content.value.length === 0) {
+      number = this.#removeCommas(content.getAttribute('placeholder'));
+      content.setAttribute('placeholder', '');
+    }
+
+    if (content.value.length > 0) {
+      number = this.#removeCommas(content.value);
+    }
+
+    const deleted = number.toString().slice(0, -1);
+    if (deleted.length === 0) {
+      this.#clearScreen();
+    }
+
+    if (deleted.length > 0) {
+      content.value = this.#addCommas(deleted) + 'overf';
+      
+      if (content.scrollWidth === this.#contentWidth) {
+        content.style.fontSize = '2rem';
+      }
+
+      content.value = this.#addCommas(deleted);
+
+      if (this.#prevNumber !== null && this.#currNumber !== null) {
+        this.#setNull();
+      }
+    }
+  }
+
+  #clearScreen() {
+    content.value = '';
+    content.setAttribute('placeholder', '');
+  }
+
+  #setNull() {
+    this.#currNumber = null;
+    this.#prevNumber = null;
+    this.#currOperation = null;
+  }
+
+  #addCommas(number) {
+    let [integer, decimal] = number.replaceAll(',', '').split('.');
+    decimal = decimal ? `.${decimal}` : '';
+    return Number(integer).toLocaleString('en') + decimal;
+  }
+
+  #removeCommas(number) {
+    if (number === '.') return 0.0;
+    return Number(number.replaceAll(',', ''));
+  }
+
+  #addContent(value) {
+    if (content.value.length === 18)
+      return this.#renderError('You have reached the maximum');
+
+    if (value === '.') {
+      content.value += '.';
     } else {
-        console.log("Not a part of calc keypad");
+      const number = content.value + value;
+      content.value = this.#addCommas(number);
+
+      if (content.scrollWidth > this.#contentWidth) {
+        content.style.fontSize = '1.5rem';
+      } else {
+        content.style.fontSize = '2rem';
+      }
     }
+  }
+
+  #doOperation(type) {
+    this.#currNumber =
+      content.value.length > 0
+        ? this.#removeCommas(content.value)
+        : this.#currNumber;
+    this.#prevNumber = Number(
+      this.#operations[type](this.#prevNumber, this.#currNumber).toFixed(8)
+    );
+
+    this.#setplaceholder();
+  }
+
+  #handleOperation(operationType) {
+    if (
+      this.#prevNumber !== null &&
+      this.#currNumber === null &&
+      content.value.length > 0
+    ) {
+      this.#doOperation(this.#currOperation);
+      this.#currOperation = operationType;
+    }
+
+    if (
+      (this.#prevNumber === null && content.value.length > 0) ||
+      content.getAttribute('placeholder').length > 0
+    ) {
+      this.#prevNumber =
+        content.value.length > 0
+          ? this.#removeCommas(content.value)
+          : this.#prevNumber;
+      this.#currNumber = null;
+      this.#currOperation = operationType;
+    }
+
+    if (content.value.length > 0) {
+      this.#setplaceholder();
+    }
+  }
+
+  #handleKeys(e) {
+    const key = e.target.closest('li');
+    if (!key) return;
+
+    if (
+      key === e.target.closest('.number') ||
+      (key.dataset.value === 'dot' && !content.value.includes('.'))
+    ) {
+      const value = key.dataset.value === 'dot' ? '.' : key.dataset.value;
+      this.#addContent(value);
+    }
+
+    if (
+      key.dataset.value === 'min' &&
+      content.value.length === 0 &&
+      content.getAttribute('placeholder').length === 0
+    ) {
+      content.value = '-';
+    }
+
+    if (key.dataset.value === 'plus' && content.value === '-') {
+      content.value = '';
+    }
+
+    if (key.dataset.value === 'del') {
+      this.#remove();
+    }
+
+    if (key.dataset.value === 'res') {
+      this.#clearScreen();
+    }
+
+    if (content.value === '.' || content.value === '-') return;
+
+    if (
+      key.dataset.value === 'eq' &&
+      this.#currOperation &&
+      (this.#currNumber !== null || content.value.length > 0)
+    ) {
+      this.#doOperation(this.#currOperation);
+    }
+
+    // Operations: +, -, x, /
+    if (Object.keys(this.#operations).includes(key.dataset.value)) {
+      this.#handleOperation(key.dataset.value);
+    }
+
+    if (
+      content.value.length === 0 &&
+      content.getAttribute('placeholder').length === 0
+    ) {
+      this.#setNull();
+    }
+  }
+
+  #bindKeyboard(e) {
+    if (Number.isInteger(+e.key) || e.key === '.') {
+      this.#addContent(e.key);
+    }
+
+    if (e.key === 'Backspace') {
+      this.#remove();
+    }
+
+    if (e.key === 'Delete') {
+      this.#clearScreen();
+      this.#setNull();
+    }
+
+    if (
+      e.key === 'Enter' &&
+      this.#currOperation &&
+      (this.#currNumber !== null || content.value.length > 0)
+    ) {
+      this.#doOperation(this.#currOperation);
+    }
+
+    if (['+', '-', '*', '/'].includes(e.key)) {
+      let operationType = Object.entries(this.#others).find(
+        entry => entry[1] === e.key
+      )?.[0];
+      operationType = operationType ?? 'mul';
+      this.#handleOperation(operationType);
+    }
+  }
 }
 
-function displayKeys(key) {
-    let regex= /[.+-/*]/;
-    keysArray.push(key);
-    if(regex.test(keysArray[0]) || (/[√]/.test(keysArray[keysArray.length-1]) && /[√]/.test(keysArray[keysArray.length-2]))){
-        keysArray.pop();
-    } else if (regex.test(keysArray[keysArray.length-1]) && regex.test(keysArray[keysArray.length-2])){
-        keysArray.pop();
-    } else{
-        document.querySelector("body > div.calculator > div.calculator-screen > p").innerHTML = keysArray.join('');
-    }
-}
-
-function deleteKey() {
-    if (keysArray.length === 0) {
-        console.log("Delete operation failed: Keys array empty");
-    } else {
-        let keyPopped = keysArray.pop();
-        console.log(`key popped: ${keyPopped}`);
-        document.querySelector("body > div.calculator > div.calculator-screen > p").innerHTML = keysArray.join('');
-    }
-}
-
-function resetKeys() {
-    keysArray = [];
-    document.querySelector("body > div.calculator > div.calculator-screen > p").innerHTML = "";
-}
-
-function getResult() {
-    let regex = /[√]/;
-    let expression = keysArray.join('');
-    keysArray = expression.split(/([+-/*√])/).filter(Boolean);
-    for(var i=0; i<keysArray.length; i++){
-        if(regex.test(keysArray[i])){
-            keysArray[i] = Math.sqrt(keysArray[i+1])
-            keysArray.splice(i+1,1);
-        }
-    }
-    let x = Function('return ' + keysArray.join(''))()
-    if (x ||  x === 0) {
-        document.querySelector("body > div.calculator > div.calculator-screen > p").innerHTML = parseFloat(x.toFixed(5));
-    }else{
-        console.log("Value's have not been entered");
-        document.querySelector("body > div.calculator > div.calculator-screen > p").innerHTML = "";  
-    }
-    keysArray = [];
-}
+new Calculator();
